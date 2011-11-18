1ac078fed9fad1851ee5e8c61252f6c442ba85e0
dan lentz <danlentz@gmail.com>
2011-11-18 10:14:25 -0500

    ironed out some api gaps and inconsistencies in basic persistent indexing api

--- contrib/elephant/phash.lisp
+++ contrib/elephant/phash.lisp
@@ -7,23 +7,21 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PHASH / PINDEX
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (def logger phash (triple-spaced.hashing::hashing))
  
 (def (class* eas) phash ()
-  ((map :accessor phash-map))
-  (:default-initargs :map (ele:make-btree)))
+  ((btree :initarg :btree :accessor phash-btree))
+  (:default-initargs :btree (ele:make-btree)))
 
 (def (function e) make-phash (&optional (name (unicly:make-v4-uuid)))
   (let ((btree (ele:get-from-root name)))
     (if btree
       (make-instance 'phash :map btree)
       (let ((phash (make-instance 'phash)))
-        (ele:add-to-root name (phash-map phash))
+        (ele:add-to-root name (phash-btree phash))
         phash))))
 
 (def (class* eas) pindex (phash)
-  ((map :accessor pindex-map))
+  ((btree :accessor pindex-btree))
   (:default-initargs :map (ele:make-indexed-btree)))
 
 (def (function e) make-pindex (&optional (name (unicly:make-v4-uuid)))
@@ -31,12 +29,9 @@
     (if btree
       (make-instance 'pindex :map btree)
       (let ((pindex (make-instance 'pindex)))
-        (ele:add-to-root name (pindex-map pindex))
+        (ele:add-to-root name (pindex-btree pindex))
         pindex))))
 
-
-(def (function e) phash-help ()
-  " (describe-btree (phash-map  (make-phash 'test-phash)))")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Indexer Protocol: PHASH
@@ -44,16 +39,16 @@
 
 (def method map-get ((phash phash) key)
   (ele:ensure-transaction ()
-    (ele:get-value key (phash-map phash))))
+    (ele:get-value key (phash-btree phash))))
 
-(def (function ed) get-phash (key phash)
+(def (function ed) phash-get (key phash)
   (map-get phash key))
 
 (def method map-put ((phash phash) key value)
   (ele:ensure-transaction ()
-    (setf (ele:get-value key (phash-map phash)) value)))
+    (setf (ele:get-value key (phash-btree phash)) value)))
 
-(def (function ed) put-phash (key value phash)
+(def (function ed) phash-put (key value phash)
   (map-put phash key value))
 
 (def method map-ensure-get ((phash phash) key otherwise-put)
@@ -62,18 +57,17 @@
       (values (map-get phash key) t)
       (values (map-put phash key otherwise-put) nil))))
 
-
-(def (function ed) get-phash* (key phash otherwise-put)
+(def (function ed) phash-get* (key phash otherwise-put)
   (map-ensure-get phash key otherwise-put))
 
 (defsetf map-get map-put)
-(defsetf get-phash put-phash)
+(defsetf phash-get phash-put)
 
 (def method map-drop ((phash phash) key)
   (ele:ensure-transaction ()
-    (remove-kv key (phash-map phash))))
+    (remove-kv key (phash-btree phash))))
 
-(def (function ed) rem-phash (key phash)
+(def (function ed) phash-drop (key phash)
   (map-drop phash key))
 
 (def method map-size ((phash phash))
@@ -82,27 +76,31 @@
       (map-btree (lambda (k v)
                    (declare (ignore k v))
                    (incf count))
-        (phash-map phash))
+        (phash-btree phash))
       count)))
 
 (def (function ed) phash-size (phash)
   (map-size phash))
   
 (def method map-p ((phash phash))
-  (map-p (phash-map phash)))
+  (map-p (phash-btree phash)))
 
 (def method map-p ((btree ele:btree))
   t)
 
 (def method map-map  ((phash phash) fn/2)
   (ele:ensure-transaction ()
-    (ele:map-btree fn/2 (phash-map phash))))
+    (ele:map-btree fn/2 (phash-btree phash))))
 
-(def (function ed) map-phash (fn phash)
+(def (function ed) phash-map (fn phash)
   (map-map phash fn))
 
-;; (def (function ed) clear-phash (phash)
-  ;;(setf (phash-map phash) (ele:make-btree)))
+(def (function ed) phash-clear (phash)
+  (setf (slot-value phash 'btree) (ele:make-btree)))
+
+(def (function ed) pindex-clear (pindex)
+  (setf (slot-value pindex 'btree) (ele:make-indexed-btree)))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Testing
@@ -116,13 +114,13 @@
   (unwind-protect
     (let ((*phash*  (make-phash 'test-phash)))
       (-body-))
-    (ele:drop-btree (phash-map (make-phash 'test-phash)))))
+    (ele:drop-btree (phash-btree (make-phash 'test-phash)))))
 
 (def test object-identity ()
   (with-fixture |phash|
     (is (not (eq (make-phash 'test-phash) (make-phash 'test-phash))))
-    (is (eq (phash-map (make-phash 'test-phash)) (phash-map (make-phash 'test-phash))))
-    (ele:drop-btree (phash-map (make-phash 'test-phash)))))
+    (is (eq (phash-btree (make-phash 'test-phash)) (phash-btree (make-phash 'test-phash))))
+    (ele:drop-btree (phash-btree (make-phash 'test-phash)))))
 
 (def test table-primitives ()
   (ele:ensure-transaction ()
