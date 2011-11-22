3b3928397c322ad153fd7b3bf80616ff24e1e829
dan lentz <danlentz@gmail.com>
2011-11-22 08:55:01 -0500

    fix cbd conflicting with internal wilbur function name

--- contrib/elephant/frame-api.lisp
+++ contrib/elephant/frame-api.lisp
@@ -64,12 +64,13 @@
 (defun frame-cbd (graph node)
   (db-find-cbd (named-graph-db graph) node))
 
+#|
+
 (defun cbd (major &optional minor)
   (if minor
     (frame-cbd major minor)
     (db-find-cbd *db* major)))
-  
-#|
+ 
 
  (with-merged-graphs (!rdf: !skos:)
    (pprint (own-slots !rdf:Statement))
