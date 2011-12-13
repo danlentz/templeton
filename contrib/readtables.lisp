f97bf6cb4e1e7953fdd8067a956a759b9347d2d2
dan lentz <danlentz@gmail.com>
2011-12-13 14:58:19 -0500

    named readtable :|standard|

---
+++ contrib/readtables.lisp
@@ -0,0 +1,10 @@
+;;;;; -*- mode: common-lisp;   common-lisp-style: modern;    coding: utf-8; -*-
+;;;;;
+;;;;; named readtables
+;;;;;
+
+
+(in-package :templeton)
+
+(defreadtable :|standard|
+  (:merge :common-lisp))
